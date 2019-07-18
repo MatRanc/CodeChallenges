25878f610dd23f7d3ee895cc02a5b056a4ac1a01
Mathieu <matranc03@gmail.com>
2019-07-18 14:55:59 -0400

    Added comments

--- howLongHaveIBeenAlive?/howLongHaveIBeenAlive?/main.m
+++ howLongHaveIBeenAlive?/howLongHaveIBeenAlive?/main.m
@@ -11,7 +11,9 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
+        //Get the date
         NSDate *now = [NSDate date];
+        //Set the components to the beginning date (birthday)
         NSDateComponents *comps = [[NSDateComponents alloc] init];
             [comps setYear:2003];
             [comps setMonth:10];
@@ -20,13 +22,17 @@ int main(int argc, const char * argv[]) {
             [comps setMinute:0];
             [comps setSecond:0];
         
+        //Get the calendar
         NSCalendar *g = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
+        //Set the date of birth using the components and the correct calendar
         NSDate *dateOfBirth = [g dateFromComponents:comps];
         
+        //Define the variables
         double secondsSinceEarlierDate = [now timeIntervalSinceDate:dateOfBirth];
         unsigned long secondsTotal = secondsSinceEarlierDate;
         double yearsSince = secondsTotal / 3.154e+7;
         
+        //Print the answer
         NSLog(@"I've been alive for %lu seconds or %0.2f years", secondsTotal, yearsSince);
 
     }
