3b59dda30ffd404fbb3e3e800a50dd0a535086b1
Mathieu <matranc03@gmail.com>
2019-07-22 21:43:49 -0400

    working on groceries

---
+++ Groceries/Groceries/main.m
@@ -0,0 +1,43 @@
+//
+//  main.m
+//  Groceries
+//
+//  Created by Mathieu Rancourt on 2019-07-22.
+//  Copyright © 2019 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        NSString *eggs = @"Eggs";
+        NSString *bacon = @"Bacon";
+        NSString *milk = @"Milk";
+        NSString *bread = @"Bread";
+        NSString *broccoli = @"Broccoli";
+        
+        NSMutableArray *groceryList = [NSMutableArray array];
+      
+        [groceryList addObject:eggs];
+        [groceryList addObject:bacon];
+        [groceryList addObject:milk];
+        [groceryList addObject:bread];
+        [groceryList addObject:broccoli];
+        
+        NSLog(@"My grocery list is:");
+        for (NSMutableArray *d in groceryList){
+            NSLog(@"%@", d);
+        }
+        
+            [groceryList removeObjectAtIndex:4];
+        NSLog(@"The grocery list is now %@", groceryList);
+        
+            
+            
+        }
+        
+        
+
+    return 0;
+}
