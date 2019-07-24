85379f82203b3316fb50773796d8351b299c7179
Mathieu <matranc03@gmail.com>
2019-07-23 20:55:04 -0400

    did the interesting names challenge, it is hard.

--- InterestingNames/InterestingNames/main.m
+++ InterestingNames/InterestingNames/main.m
@@ -10,21 +10,49 @@
 int main (int argc, const char * argv[])
 {
     @autoreleasepool {
+        
         // Read in a file as a huge string (ignoring the possibility of an error)
         NSString *nameString =
         [NSString stringWithContentsOfFile:@"/usr/share/dict/propernames"
                                   encoding:NSUTF8StringEncoding
                                      error:NULL];
-        // Break it into an array of strings
+        NSString *wordString =
+        [NSString stringWithContentsOfFile:@"/usr/share/dict/words"
+                                  encoding:NSUTF8StringEncoding
+                                     error:NULL];
+       
+        //Turn that into an array
         NSArray *names = [nameString componentsSeparatedByString:@"\n"];
-        // Go through the array one string at a time
-        for (NSString *n in names) {
-            // Look for the string "aa" in a case-insensitive manner
-            NSRange r = [n rangeOfString:@"AA" options:NSCaseInsensitiveSearch];
-            // Was it found?
-            if (r.location != NSNotFound) {
-                NSLog(@"%@", n);
+        NSArray *words = [wordString componentsSeparatedByString:@"\n"];
+        
+        
+        //Setup the string name to be refrencing the names array
+        for (NSString *name in names){
+            //Setup tje string word to be refrencing the array words
+            for (NSString *word in words){
+                //If the name is equal to a word, print the word and the name
+                if ([name isEqualToString:word]){
+                    NSLog(@"The name %@ is also the word %@", name, word);
+                }
             }
         }
     }
+    
     return 0; }
+
+
+
+
+
+/* I did need help for this one as I could not understand where to start. This one works but I've modified it above. (Annotations by me)
+ //Setup the word find in names
+ for (NSString *validNames in names) {
+ //Setup the exact same word but in word list
+ for (NSString *validWords in words) {
+ //If the name is equal to the word then print the name and word
+ if ([validNames isEqualToString:validWords]) {
+ NSLog(@"%@ is the same as %@", validNames, validWords);
+ }
+ }
+ }
+ */
